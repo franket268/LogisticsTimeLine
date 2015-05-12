e641548faa488848af505a95ae5a6a37637eff9c
GongZhanhong <zhanhong.gong@imaygou.com>
2015-05-13 01:30:39 +0800

    modify sample

--- sample/src/main/java/com/gzh/logisticstimeline/MainActivity.java
+++ sample/src/main/java/com/gzh/logisticstimeline/MainActivity.java
@@ -13,10 +13,17 @@ public class MainActivity extends ActionBarActivity {
     setContentView(R.layout.activity_main);
     LogisticsTimeline timeline = (LogisticsTimeline) findViewById(R.id.logistic_timeline);
 
-    CharSequence[] logisticsDesc = {"arrived one city","arrived new city","mall shipped"};
-    CharSequence[] date ={"2015-4-12","2015-4-11","2015-3-1"};
+    CharSequence[] logisticsDesc = {"arrived one city","arrived new city","mall shipped","order submit"};
+    CharSequence[] date ={"2015-4-12","2015-4-11","2015-3-1","2015-2-1"};
     timeline.setDescArray(logisticsDesc);
     timeline.setDateArray(date);
+
+    LogisticsTimeline timeline2 = (LogisticsTimeline) findViewById(R.id.logistic_timeline2);
+    CharSequence[] logisticsDesc2 = {"arrived one city","mall shipped","order submit"};
+    CharSequence[] date2 ={"2015-4-12","2015-3-1","2015-2-1"};
+    timeline2.setDescArray(logisticsDesc2);
+    timeline2.setDateArray(date2);
+
   }
 
 }
